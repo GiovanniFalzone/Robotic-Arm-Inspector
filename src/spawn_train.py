41c98574860d2d9b4cc75c75918909fd31307d3e
robertociardi <roberto.ciardi93@gmail.com>
2019-03-18 12:37:45 +0100

    merge of spawn_train and test_arm to have a unique user program

--- src/spawn_train.py
+++ src/spawn_train.py
@@ -5,17 +5,23 @@ from gazebo_msgs.srv import DeleteModel, SpawnModel, SpawnModelRequest, SpawnMod
 from std_msgs.msg import String													# to exchange string messages
 from copy import deepcopy
 from xml.dom import minidom														# to parse sdf model
+from math import pi
 import json
 import random
+import ast
 
+from train_inspector import *
 from train_model_parser import *
 
-train_model = train_model()
+train_model = train_model()				# parser to work on train sdf and train dictionary
+motion_lib = robot_motion_lib()			# motion lib for ur5
+inspector = train_inspector()			# train inspector to "visit" the train
 
 # global variables
 x0 = 0.0
 y0 = 0.0
 z0 = 2.0
+model_id = 0
 
 # a pad is considered "good" or "bad" w.r.t. its width
 good_pad_width = 0.1		
@@ -30,8 +36,8 @@ def find_pad_position(dir, pw):
 	and the disks position as: disk_center +- half_disk +- pad width"""
 	y = 0
 
-	axis_width = train_model.train_struct_get_width("axis")
-	disk_width = train_model.train_struct_get_width("disks")
+	axis_width = train_model.train_struct_get_width('axis')
+	disk_width = train_model.train_struct_get_width('disks')
 
 	if "left" in dir:
 		y = axis_width / 3 + disk_width / 2
@@ -46,13 +52,14 @@ def set_pads_width(left_pad_width, right_pad_width):
 	"""Modify pads width in train_structure and in sdf model, to show on gazebo"""
 	links = train_model.sdf_get_links()
 
-	train_model.train_struct_set_pose("pads", [left_pad_width, right_pad_width])
+	train_model.train_struct_set_width('pads', left_pad_width, 'left')
+	train_model.train_struct_set_width('pads', right_pad_width, 'right')
 
 	#iterate on links
 	for elem in links:
 		# check if name attribute is "left_pad" or "right_pad"
 		if train_model.sdf_check_name_attribute(elem, "left_pad"):
-			# get y coordinates for left pad
+			# get new position coordinates for left pad
 			pad_pose_value = find_pad_position("left", left_pad_width)
 				
 			# get "pose" element from sdf and modify its value
@@ -73,9 +80,7 @@ def set_pads_width(left_pad_width, right_pad_width):
 			else:
 				train_model.sdf_set_pad_color(elem, "Red")
 
-			# modify "train_structure" dictionary
-			train_model.train_struct_set_width("pads", "left", left_pad_width)
-			train_model.train_struct_set_left_pose("pads", pad_pose_value)
+			train_model.train_struct_set_left_pose('pads', pad_pose_value)
 			
 		elif train_model.sdf_check_name_attribute(elem, "right_pad"):
 			pad_pose_value = find_pad_position("right", right_pad_width)
@@ -96,31 +101,7 @@ def set_pads_width(left_pad_width, right_pad_width):
 			else:
 				train_model.sdf_set_pad_color(elem, "Red")
 
-			train_model.train_struct_set_width("pads", "right", right_pad_width)
-			train_model.train_struct_set_right_pose("pads", pad_pose_value)
-
-# creation of message for init inspector		
-def create_message(command):
-	message = { 
-		'cmd': command,
-		'train_description':{
-			'axis':{
-				'position': train_model.train_struct_get_coordinates("axis"),
-				'lenght': train_model.train_struct_get_width("axis"),
-				'pads':{
-					'num': 2,
-					'coordinates':[
-						train_model.train_struct_get_coordinates("pads")[0],
-						train_model.train_struct_get_coordinates("pads")[1]
-					],
-					'disk':{
-						'radius': train_model.train_struct_get_radius("disks"),
-					},
-				}
-			}
-		}
-	}
-	return message
+			train_model.train_struct_set_right_pose('pads', pad_pose_value)
 
 def create_train_request(modelname, px, py, pz, lpw, rpw):
 	"""Create a SpawnModelRequest with the parameters of the train given.
@@ -130,6 +111,7 @@ def create_train_request(modelname, px, py, pz, lpw, rpw):
 	rpw: right pad width"""
 	
 	set_pads_width(lpw,rpw)
+	train_model.train_struct_set_name(modelname)
 
 	train = deepcopy(train_model.get_sdf())
 
@@ -142,17 +124,169 @@ def create_train_request(modelname, px, py, pz, lpw, rpw):
 
 	return req
 
-def spawn_notify(pub):
-	if not rospy.is_shutdown():
-		msg = create_message("check")
-		json_msg = json.dumps(msg)
-		#print json_msg
-		pub.publish(json_msg)
+def spawn_train(spawn_srv, delete_srv):
+	"""Interacts with user to spawn a train, specifying pads specs. It requests
+	spawn_srv: the ros proxy to spawn model on gazebo
+	delete_srv: the ros proxy to delete model from gazebo"""
+	
+	out_msg = 'Press \n\
+\t-> 1 to spawn a train with "good" pads \n\
+\t-> 2 to spawn a train with "bad" pads \n\
+\t-> 3 to spawn a train with random pads \n\
+\t-> 4 to spawn a train with chosen pads width \n\
+\t-> c to continue (train already spawned)\n'
+
+	global model_id
+
+	in_cmd = raw_input(out_msg)
+	model_id +=1
+	if '1' in in_cmd:
+		# Spawn train with good pads
+		print "Spawning train with good pads"
+		req = create_train_request("train_model_"+str(model_id), 		# model name
+									0.0, 0.0, 0.0,		# train initial position
+									good_pad_width, good_pad_width)  		# pads length (status)
+		spawn_srv.call(req)
+	elif '2' in in_cmd:
+		print "Spawning train with bad pads"
+		req = create_train_request("train_model_"+str(model_id), 
+									0.0, 0.0, 0.0,
+									bad_pad_width, bad_pad_width) 
+		spawn_srv.call(req)
+	elif '3' in in_cmd:
+		print "Spawning train with random pads width"
+		lpw = random.uniform(bad_pad_width, good_pad_width)
+		print "-left pad width "+str(lpw)
+		rpw = random.uniform(bad_pad_width, good_pad_width)
+		print "-right pad width "+str(rpw)
+		req = create_train_request("train_model_"+str(model_id), 
+									0.0, 0.0, 0.0,
+									lpw, rpw) 
+		spawn_srv.call(req)
+	elif '4' in in_cmd:
+		out_msg = 'Choose pads width for left pad\n'
+		lpw = float(raw_input(out_msg))
+		out_msg = 'Choose pads width for right pad\n'
+		rpw = float(raw_input(out_msg))
+		print "Spawning train with chosen pads width:"
+		print "-left pad width "+str(lpw)
+		print "-right pad width "+str(rpw)
+		req = create_train_request("train_model_"+str(model_id),
+									0.0, 0.0, 0.0,
+									lpw, rpw)
+		spawn_srv.call(req)	
+	elif 'c' in in_cmd:
+		print train_model.train_structure
+		return 1	
+
+	while True:
+		conf_msg = 'Press \n\
+	\t-> 0 to delete the spawned train \n\
+	\t-> p to print train specs \n\
+	\t-> c to confirm and continue \n'
+		in_cmd = raw_input(conf_msg)
+
+		if '0' in in_cmd:
+			print "Deleting train model"
+			delete_srv("train_model_"+str(model_id))
+			return 0
+		elif 'p' in in_cmd:
+			train_model.train_struct_print()
+		else:
+			return 1
+
+def get_number(msg):
+	text = raw_input(msg)
+	return eval(text)
+
+def get_vector(msg):
+	text = raw_input(msg + ' ([x,y,z]):')
+	coords = text.split(']')[0].split('[')[1].split(',')
+	x = eval(coords[0])
+	y = eval(coords[1])
+	z = eval(coords[2])
+	return [x, y, z]
+
+def get_position():
+	return get_vector('Insert coordinates')
+
+def inspect_train():
+	inspector.set_train_description(train_model.train_structure)
+
+	msg = 'Press \n\
+\t-> 1 to move in initial position \n\
+\t-> 2 to move in max estension position \n\
+\t-> 3 to move in checking position \n\
+\t-> line to move following a cone base \n\
+\t-> circle to move following a circle \n\
+\t-> cone to move in checking position \n\
+\t-> axis to check axis\n\
+\t-> pad to check pads\n\
+\t-> check to check the train\n'
+	cmd = raw_input(msg)
+	if('1' in cmd):
+		inspector.move_in_sleep_position()
+	elif('2' in cmd):
+		inspector.move_in_waiting_position()
+	elif('3' in cmd):
+		inspector.move_in_checking_position()
+	elif('circle' in cmd):
+		axis = raw_input('which axis?(x, y or z)') 
+		vect_pos = get_position()
+		print('Circle center: ' + str(vect_pos))
+		if('x' in axis):
+			motion_lib.follow_circle_x(vect_pos)
+		elif('y' in axis):
+			motion_lib.follow_circle_y(vect_pos)
+		elif('z' in axis):
+			motion_lib.follow_circle_z(vect_pos)
+
+	elif('cone' in cmd):
+		axis = raw_input('which axis?(x, y or z)') 
+		vect_pos = get_position()
+		print('Cone vertex: ' + str(vect_pos))
+		if('x' in axis):
+			motion_lib.follow_cone_base_x(vect_pos)
+		elif('y' in axis):
+			motion_lib.follow_cone_base_y(vect_pos)
+		elif('z' in axis):
+			motion_lib.follow_cone_base_z(vect_pos)
+
+	elif('line' in cmd):
+		vect_pos = get_position()
+		steps = get_number('Insert How many step:')
+		direction = get_vector('Insert Direction (Delta for each axis)')
+		motion_lib.follow_line(vect_pos, direction, steps)
+	elif('train_axis' in cmd):
+		inspector.inspect_axis()
+	elif('train_pads' in cmd):
+		inspector.inspect_pads()
+	elif('pos' in cmd):
+		vect_pos = get_position()
+		vect_pos.extend([0,0,0]),
+		print('moving to: ' + str(vect_pos))
+		motion_lib.move_in_xyz_rpy(vect_pos)
+
+	elif('rotate' in cmd):
+		vect = get_vector('Insert rpy as: ')
+		motion_lib.rotate(vect)
+
+	elif('pad' in cmd):
+		inspector.inspect_pads()
+
+	elif('axis' in cmd):
+		inspector.inspect_axis()
+
+	elif('check' in cmd):
+		inspector.check_train(train_model.train_structure)
+
+	else:
+		print('Wrong input \n')
 
 if __name__ == '__main__':
 
 	# connection to spawn_sdf_model service and to delete_model service of gazebo
-	rospy.init_node('spawn_models', anonymous=True)
+	#rospy.init_node('spawn_models', anonymous=True)
 	delete_srv = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 	rospy.loginfo("Waiting for /gazebo/delete_model service...")
 	delete_srv.wait_for_service()
@@ -163,69 +297,23 @@ if __name__ == '__main__':
 	spawn_srv.wait_for_service()
 	rospy.loginfo("Connected to spawn service!")
 
-	pub = rospy.Publisher('spawn_channel', String, queue_size=10)
-	
-	i = 0
-
-	while True:
-		out_msg = 'Press \n\
-\t-> 1 to spawn a train with "good" pads \n\
-\t-> 2 to spawn a train with "bad" pads \n\
-\t-> 3 to spawn a train with random pads \n\
-\t-> 4 to spawn a train with chosen pads width \n\
-\t-> 0 to delete the spawned train \n\
-\t-> p to print train specs \n\
-\t-> other to exit \n'
-		in_cmd = raw_input(out_msg)
-		if '1' in in_cmd:
-			i+=1
-			# Spawn train with good pads
-			print "Spawning train with good pads"
-			req = create_train_request("train_model_"+str(i), 		# model name
-										0.0, 0.0, 0.0,		# train initial position
-										good_pad_width, good_pad_width)  		# pads length (status)
-			spawn_srv.call(req)
-		elif '2' in in_cmd:
-			i+=1
-			print "Spawning train with bad pads"
-			req = create_train_request("train_model_"+str(i), 
-										0.0, 0.0, 0.0,
-										bad_pad_width, bad_pad_width) 
-			spawn_srv.call(req)
-		elif '3' in in_cmd:
-			i+=1
-			print "Spawning train with random pads width"
-			lpw = random.uniform(bad_pad_width, good_pad_width)
-			print "-left pad width "+str(lpw)
-			rpw = random.uniform(bad_pad_width, good_pad_width)
-			print "-right pad width "+str(rpw)
-			req = create_train_request("train_model_"+str(i), 
-										0.0, 0.0, 0.0,
-										lpw, rpw) 
-			spawn_srv.call(req)
-		elif '4' in in_cmd:
-			out_msg = 'Choose pads width for left pad\n'
-			lpw = raw_input(out_msg)
-			out_msg = 'Choose pads width for right pad\n'
-			rpw = raw_input(out_msg)
-			print "Spawning train with chosen pads width:"
-			print "-left pad width "+str(lpw)
-			print "-right pad width "+str(rpw)
-			req = create_train_request("train_model_"+str(i),
-										0.0, 0.0, 0.0,
-										lpw, rpw)
-			spawn_srv.call(req)		
-		elif '0' in in_cmd:
-			print "Deleting train model"
-			delete_srv("train_model_"+str(i))
-			continue
-		elif 'p' in in_cmd:
-			train_model.train_struct_print()
-		else:	
+	cont = 0
+	while cont == 0:
+		try:
+			cont = spawn_train(spawn_srv, delete_srv)
+		except rospy.ROSInterruptException:
+			print('rospy exception')
+			break
+		except KeyboardInterrupt:
+			print('keyboard interrupt')
 			break
 		
-		# notify the spawn on the channel
+	while True:
 		try:
-			spawn_notify(pub)
+			inspect_train()
 		except rospy.ROSInterruptException:
-			pass
+			print('rospy exception')
+			break
+		except KeyboardInterrupt:
+			print('keyboard interrupt')
+			break
