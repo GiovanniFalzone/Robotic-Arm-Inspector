74911b78407722673eb5466d700f1a448fe724ab
GiovanniFalzone <falzone.giovanni2@gmail.com>
2019-03-19 23:05:49 +0100

    add explore_world

--- src/train_inspector.py
+++ src/train_inspector.py
@@ -93,18 +93,11 @@ class train_inspector():
 		self.motion_lib.move_in_xyz_rpy(pos)
 
 	def inspect_pads_cone(self):
-		pads_pos = self.train_description.get('axis').get('pads').get('coordinates')
-		dist = self.train_description.get('axis').get('pads').get('disk').get('radius') + 0.4
-		print(pads_pos)
-		for pos in pads_pos:
-			pc1 = copy.deepcopy(self.last_Kinect_PC2)
-			pos1 = self.motion_lib.get_pos_xyz_rpy()
-			self.motion_lib.follow_cone_base_z(pos, 0.1, dist, 2*math.pi, 1, 5)
-			while(pc1.header.stamp == self.last_Kinect_PC2.header.stamp):
-				pass
-			pc2 = copy.deepcopy(self.last_Kinect_PC2)
-			pos2 = self.motion_lib.get_pos_xyz_rpy()
-			self.send_to_PC_checker(pc1, pos1, pc2, pos2)
+		left_pad = self.train_description.get('pads').get('coordinates').get('left')
+		right_pad = self.train_description.get('pads').get('coordinates').get('right')
+		dist = self.train_description.get('disks').get('radius') + 0.4
+		self.motion_lib.follow_cone_base_z(left_pad, 0.1, dist, 2*math.pi, 1, 5)
+		self.motion_lib.follow_cone_base_z(right_pad, 0.1, dist, 2*math.pi, 1, 5)
 		
 	def inspect_pads(self):
 		pc1 = copy.deepcopy(self.last_Kinect_PC2)
@@ -156,6 +149,56 @@ class train_inspector():
 	def check_train(self, train_desc):
 		self.move_in_sleep_position()
 		self.move_in_waiting_position()
-		self.inspect_axis()
+		self.explore_world()
 		self.inspect_pads()
+
+	def explore_world(self):
+		# move under train and apply cone inspection
+		axis_center_pos = deepcopy(self.train_description.get('axis').get('coordinates'))
+		axis_lenght = self.train_description.get('axis').get('width')
+		dist = self.train_description.get('disks').get('radius') + 0.5
+		start_pos = axis_center_pos
+		axis_border_offset = axis_lenght/3
+		start_pos[1] -= axis_border_offset
+		start_pos[2] -= dist
+		n_step = 3
+		step = (float(axis_lenght-2*axis_border_offset)/float(n_step))
+
+		sensor_dir = [math.pi/2, math.pi/2, math.pi/2]
+
+		#check first half plane, joint is limited to move within pi
+		for i in range(0,5):
+			joints_vect = [0, -i*math.pi/4, 0, -math.pi/2, 0, 0]
+			self.motion_lib.set_joint(joints_vect)
+
+		#check second half plane
+		for i in range(0,5):
+			joints_vect = [0, i*math.pi/4, 0, -math.pi/2, 0, 0]
+			self.motion_lib.set_joint(joints_vect)
+
+		# now in a smaller concentric circle
+		#check first half plane, joint is limited to move within pi
+		for i in range(0,5):
+			joints_vect = [0, -i*math.pi/4, -math.pi/2, -math.pi/2, 0, 0]
+			self.motion_lib.set_joint(joints_vect)
+
+		#check second half plane
+		for i in range(0,5):
+			joints_vect = [0, i*math.pi/4, -math.pi/2, -math.pi/2, 0, 0]
+			self.motion_lib.set_joint(joints_vect)
+
+
+		self.move_in_sleep_position()
+
+		#check under axis
+		for i in range(0, n_step):
+			x = start_pos[0]
+			y = start_pos[1] + i*step
+			z = start_pos[2]
+			pos = [x, y, z]
+			pos.extend(sensor_dir)
+			self.motion_lib.move_in_xyz_rpy(pos)
+
+		#check pads
+		self.inspect_pads_cone()
 
